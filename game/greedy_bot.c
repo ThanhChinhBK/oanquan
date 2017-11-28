4b8476f8a634adf7c7cf1421bae7b47575f5b992
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2017-11-28 22:23:51 +0700

    fix fucking point

--- game/greedy_bot.c
+++ game/greedy_bot.c
@@ -1,15 +1,22 @@
 #include "game.h"
 #include "board.h"
+#include <string.h>
 
 int greedy(int board[]){
   int optinum = -1, total_step, optinum_score=-1;
   int list_step[20];
   int i, tmp_score;
+  int copy_board[12];
   total_step =  get_list_step_true(board, 0, list_step);
   for(i=0; i < total_step; i++){
-    tmp_score = move_iter(board, list_step[i], 0);
-    if(optinum == -1 || optinum_score < tmp_score)
+    memcpy(copy_board, board, sizeof(int)*12);
+    tmp_score = move_iter(copy_board, list_step[i], 0);
+    if(optinum == -1 || optinum_score < tmp_score){
       optinum = list_step[i];
+      optinum_score = tmp_score;
+    }
+
+    
   }
   return optinum;
 }
